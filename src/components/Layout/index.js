09ddcfcc90ecd0b9acc6277c1b118e15c874da69
Aleksandar <ilicca96@gmail.com>
2024-03-31 13:18:37 +0200

    add sidebar

---
+++ src/components/Layout/index.js
@@ -0,0 +1,8 @@
+import Sidebar from '../Sidebar'
+import './index.less'
+
+const Layout = () => {
+    return <><Sidebar /></>
+}
+
+export default Layout
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,53 @@
+import './index.css'
+import LogoS from '../../assets/images/logo-a.jpg'
+import LogoSub from '../../assets/images/logo_sub.png'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+
+const Sidebar = () => (
+    <>
+        <div className="nav-bar">
+            <Link className="logo" to="/">
+                <img src={LogoS} alt="logo" />
+                <img className="sub-logo" src={LogoSub} alt="logo" />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+
+            </nav>
+
+            <ul>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/aleksandar-ilić-5bab01181">
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://github.com/ilicca/">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noreferrer" href="https://www.facebook.com/aleksandar.c.ilic.7/">
+                        <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </>
+)
+
+export default Sidebar
