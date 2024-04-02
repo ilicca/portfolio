e298d9bca1353556eaa215dc049d3fd21c0fd3f2
Aleksandar <ilicca96@gmail.com>
2024-04-02 20:52:23 +0200

    replace gsap with animejs

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,35 +1,35 @@
-import './index.css'
-import LogoA from '../../../assets/images/logo-a.png'
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import React, { useEffect, useRef } from 'react';
+import anime from 'animejs/lib/anime.es.js';
+import LogoA from '../../../assets/images/logo-a.png';
+import './index.css';
 
 const Logo = () => {
-
-    const bgRef = useRef()
-    const outlineLogoRef = useRef()
-    const solidLogoRef = useRef()
+    const bgRef = useRef();
+    const outlineLogoRef = useRef();
+    const solidLogoRef = useRef();
 
     useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
+        const tl = anime.timeline({ autoplay: false });
 
-        gsap.timeline()
-        .to(bgRef.current, {
-            duration: 1,
+        tl.add({
+            targets: bgRef.current,
             opacity: 1,
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
-        gsap.fromTo(solidLogoRef.current, {
-            opacity: 0,
-        }, {
+            duration: 1000,
+        }).add({
+            targets: outlineLogoRef.current,
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutSine',
+            duration: 20000,
+        }, '-=1000').add({
+            targets: solidLogoRef.current,
             opacity: 1,
-            delay: 4, 
-            duration: 4,
-        })
-    }, [])
+            duration: 4000,
+            delay: 4000,
+        });
+
+        tl.play();
+    }, []);
+
     return (
         <div className="logo-container" ref={bgRef}>
             <img ref={solidLogoRef} className="solid-logo" src={LogoA} alt="A" />
@@ -38,10 +38,10 @@ const Logo = () => {
                 preserveAspectRatio="xMidYMid meet">
 
                 <g className="svg-container"
-                transform="translate(0.000000,462.000000) scale(0.100000,-0.100000)"
+                    transform="translate(0.000000,462.000000) scale(0.100000,-0.100000)"
                     fill="#000000" stroke="none">
-                    <path ref={outlineLogoRef} 
-                    d="M1300 4519 c-30 -5 -61 -16 -69 -24 -24 -24 -292 -1114 -471 -1920
+                    <path ref={outlineLogoRef}
+                        d="M1300 4519 c-30 -5 -61 -16 -69 -24 -24 -24 -292 -1114 -471 -1920
 -76 -345 -125 -580 -272 -1324 l-13 -64 -195 -47 -195 -48 2 -198 c2 -233 17
 -430 43 -566 25 -130 30 -138 85 -138 43 0 45 -1 55 -39 5 -21 16 -43 23 -48
 15 -13 1757 63 1770 76 5 5 4 89 -2 202 -16 277 -42 652 -46 667 -3 6 -18 12
@@ -60,15 +60,15 @@ const Logo = () => {
 -392 1 -1 92 -14 202 -28 109 -15 200 -28 202 -29 10 -11 59 -834 50 -844 -8
 -7 -1738 -85 -1745 -79 -9 10 -34 123 -47 221 -16 124 -42 603 -33 626 4 10
 66 29 192 59 103 24 190 47 195 50 5 3 30 118 55 255 88 465 238 1165 396
-1835 106 449 297 1211 307 1221 26 26 318 36 588 19z"/>
+1835 106 449 297 1211 307 1221 26 26 318 36 588 19z" />
                     <path d="M1851 3023 c-17 -59 -69 -238 -115 -398 l-84 -290 37 -3 c52 -3 479
 23 487 31 6 6 -272 744 -286 760 -4 4 -21 -41 -39 -100z m214 -412 c47 -123
 83 -227 80 -230 -7 -6 -305 -24 -311 -18 -2 2 25 104 61 228 73 250 74 254 80
-248 2 -3 42 -105 90 -228z"/>
+248 2 -3 42 -105 90 -228z" />
                 </g>
             </svg>
         </div>
-    )
+    );
 }
 
-export default Logo
+export default Logo;
